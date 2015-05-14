e7b7be415f22808aa19a6076bdc0f8ba2547f25b
Radek 'blufor' Slavicinsky <radek@blufor.cz>
2015-05-14 12:12:08 +0200

    one more...

--- includes/maestro.mk
+++ includes/maestro.mk
@@ -1,11 +1,11 @@
 ${pip}:
-  $(info Installing pip)
-  wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py -O/tmp/get-pip.py
-  python /tmp/get-pip.py
-  rm -f /tmp/get-pip.py
-  touch ${pip}
+	$(info Installing pip)
+	wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py -O/tmp/get-pip.py
+	python /tmp/get-pip.py
+	rm -f /tmp/get-pip.py
+	touch ${pip}
 
 ${maestro}: ${pip}
-  $(info Installing Maestro NG)
-  ${pip} install --upgrade git+git://github.com/signalfuse/maestro-ng
-  touch ${maestro}
+	$(info Installing Maestro NG)
+	${pip} install --upgrade git+git://github.com/signalfuse/maestro-ng
+	touch ${maestro}
