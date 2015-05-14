e6a9c03d9d55d96e6ab63992684e4e179bcf43a8
Radek 'blufor' Slavicinsky <radek@blufor.cz>
2015-05-14 12:37:12 +0200

    maestro fix

--- includes/maestro.mk
+++ includes/maestro.mk
@@ -1,11 +1,11 @@
 ${pip}:
 	$(info Installing pip)
-	wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py -O/tmp/get-pip.py
+	wget -q https://raw.github.com/pypa/pip/master/contrib/get-pip.py -O/tmp/get-pip.py
 	python /tmp/get-pip.py
 	rm -f /tmp/get-pip.py
 	touch ${pip}
 
 ${maestro}: ${pip}
 	$(info Installing Maestro NG)
-	${pip} install --upgrade git+git://github.com/signalfuse/maestro-ng
+	${pip} install -q --upgrade git+git://github.com/signalfuse/maestro-ng
 	touch ${maestro}
