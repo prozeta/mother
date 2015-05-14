b3f5aefd54f1def5e37717a4bc830f6d2d721f3f
Radek 'blufor' Slavicinsky <radek@blufor.cz>
2015-05-14 17:09:44 +0200

    syntax fix

--- includes/docker_images.mk
+++ includes/docker_images.mk
@@ -2,17 +2,17 @@ ${build_puppetmaster}: ${buildpath}
 	git clone https://github.com/prozeta/mother-puppetmaster.git ${buildpath}/puppetmaster
 	cd ${buildpath}/puppetmaster
 	docker build --rm -t "mother/puppetmaster:latest" .
-	docker export "mother/puppetmaster:latest" -o ${build_puppetmaster}
+	docker export -o ${build_puppetmaster} "mother/puppetmaster:latest"
 
 ${build_puppetdb}: ${buildpath}
 	git clone https://github.com/prozeta/mother-puppetdb.git ${buildpath}/puppetdb
 	cd ${buildpath}/puppetdb
 	docker build --rm -t "mother/puppetdb:latest" .
-	docker export "mother/puppetdb:latest" -o ${build_puppetdb}
+	docker export -o ${build_puppetdb} "mother/puppetdb:latest"
 
 ${build_foreman}: ${buildpath}
 	git clone https://github.com/prozeta/mother-foreman.git ${buildpath}/foreman
 	cd ${buildpath}/foreman
 	docker build --rm -t "mother/foreman:latest" .
-	docker export "mother/foreman:latest" -o ${build_foreman}
+	docker export -o ${build_foreman} "mother/foreman:latest"
 
