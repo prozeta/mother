606c0e55ad0d4d956a1a23f8352dd5ff34745ee4
Radek 'blufor' Slavicinsky <radek@blufor.cz>
2015-05-14 17:06:47 +0200

    work

--- includes/docker_images.mk
+++ includes/docker_images.mk
@@ -1,9 +1,18 @@
-${puppetmaster}: ${buildpath}
+${build_puppetmaster}: ${buildpath}
 	git clone https://github.com/prozeta/mother-puppetmaster.git ${buildpath}/puppetmaster
+	cd ${buildpath}/puppetmaster
+	docker build --rm -t "mother/puppetmaster:latest" .
+	docker export "mother/puppetmaster:latest" -o ${build_puppetmaster}
 
-${puppetdb}: ${buildpath}
+${build_puppetdb}: ${buildpath}
 	git clone https://github.com/prozeta/mother-puppetdb.git ${buildpath}/puppetdb
+	cd ${buildpath}/puppetdb
+	docker build --rm -t "mother/puppetdb:latest" .
+	docker export "mother/puppetdb:latest" -o ${build_puppetdb}
 
-${foreman}: ${buildpath}
+${build_foreman}: ${buildpath}
 	git clone https://github.com/prozeta/mother-foreman.git ${buildpath}/foreman
+	cd ${buildpath}/foreman
+	docker build --rm -t "mother/foreman:latest" .
+	docker export "mother/foreman:latest" -o ${build_foreman}
 
--- includes/targets.mk
+++ includes/targets.mk
@@ -7,6 +7,9 @@ pip := /usr/local/bin/pip
 docker := /usr/bin/docker
 docker_compose := /usr/local/bin/docker-compose
 docker_registry := ${docker_dir}/registry
+build_puppetmaster := ${docker_dir}/images/puppetmaster.tar
+build_puppetdb := ${docker_dir}/images/puppetdb.tar
+build_foreman := ${docker_dir}/images/foreman.tar
 maestro := /usr/local/bin/maestro
 named := /usr/sbin/named
 dhcpd := /usr/sbin/dhcpd
@@ -14,6 +17,8 @@ nsupdate := /usr/bin/nsupdate
 nginx := /usr/sbin/nginx
 postgres := /usr/lib/postgresql/9.1/bin/postgres
 foreman_proxy := /usr/share/foreman-proxy/bin/smart-proxy
+
+
 
 # phony targets
 all: prepare nginx postgres docker docker_registry maestro dhcp dns
@@ -25,6 +30,7 @@ nginx: ${nginx}
 postgres: ${postgres}
 docker: ${docker} ${docker_dir} ${docker_compose}
 docker_registry: ${docker_registry}
+docker_images: ${puppetmaster} ${puppetdb} ${foreman}
 maestro: ${pip} ${maestro}
 dns: ${named}
 dhcp: ${dhcpd}
