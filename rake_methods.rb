cbf49f1bf59dd85353113e5679fccb94dce3d5fe
Radek 'blufor' Slavicinsky <radek@blufor.cz>
2015-06-08 13:58:52 +0200

    ...

--- rake_methods.rb
+++ rake_methods.rb
@@ -1,8 +1,7 @@
 def runcmd *args
   cmd = args.join(' ')
-  puts ('Running: ' + cmd).green
   begin
-    system "bash -c '#{cmd}'"
+    system "bash -c '#{cmd} >> /tmp/mother_install.log 2>&1'"
   rescue Exception => e
     puts ('Failed: ' + cmd).red
     puts e.message.red
@@ -11,7 +10,6 @@ end
 
 def deb_install *args
   pkgs = args.join(' ')
-  puts ('Installing: ' + pkgs).green
   begin
     system "bash -c 'DEBIAN_FRONTEND=noninteractive apt-get -qqy install #{pkgs}'"
   rescue Exception => e
