85ef1cf11c7d72ac0ce2dc47faeb607df59e7871
Radek 'blufor' Slavicinsky <radek@blufor.cz>
2015-06-09 14:48:37 +0200

    ...

--- lib/bonding.rb
+++ lib/bonding.rb
@@ -21,7 +21,7 @@ class Bonding
     @mask = IPAddr.new('255.255.255.255').mask(cidr).to_s
   end
 
-  def conf
+  def conf_bond
     "# created automatically by Project Mother
 auto #{self.bond_if}
 iface bond0 inet static
@@ -31,11 +31,22 @@ iface bond0 inet static
     bond_mode active-backup
     bond_miimon 100
     bond_downdelay 200
-    bond_updelay 200"
+    bond_updelay 200\n\n"
+  end
+
+  def conf_eth
+    out = ""
+    self.slave_ifs.each do |iface|
+      out += "# created automatically by Project Mother
+auto #{iface}
+iface #{iface} inet manual
+    bond-master #{self.bond_if}\n\n"
+    end
+    out
   end
 
   def write
-    File.open("/etc/network/interfaces.d/#{self.bond_if}", "w") { |f| f << self.conf }
+    File.open("/etc/network/interfaces.d/#{self.bond_if}", "w") { |f| f << self.conf_eth + self.conf_bond }
   end
 
 end
