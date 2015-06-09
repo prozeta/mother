2fe9415c884a3ab68db55a86d44236820cb30d6d
Radek 'blufor' Slavicinsky <radek@blufor.cz>
2015-06-09 11:41:54 +0200

    ...

--- lib/rake_methods.rb
+++ lib/rake_methods.rb
@@ -11,14 +11,13 @@ def runcmd *args
   cmd = args.join(' ')
   begin
     cmdlog "start -> " + cmd
-    system "bash -c '#{cmd}' >> /tmp/mother_cmds.log 2>&1"
-    if $? > 0
+    if system "bash -c '#{cmd}' >> /tmp/mother_cmds.log 2>&1"
+      cmdlog "success -> " + cmd
+    else
       err "Failed running command: " + cmd
       cmdlog "fail -> " + cmd
       warn "Check /tmp/mother_cmds.log for output"
       exit 1
-    else
-      cmdlog "success -> " + cmd
     end
   rescue Exception => e
     err "Failed running command: " + cmd
@@ -33,15 +32,14 @@ def deb_install *args
   pkgs = args.join(' ')
   begin
     aptlog "install -> " + pkgs
-    system "bash -c 'DEBIAN_FRONTEND=noninteractive apt-get -y install #{pkgs} >> /tmp/mother_apt.log 2>&1'"
-    info "Installed DEBs: " + pkgs
-    if $? > 0
+    if system "bash -c 'DEBIAN_FRONTEND=noninteractive apt-get -y install #{pkgs} >> /tmp/mother_apt.log 2>&1'"
+      info "Installed DEBs: " + pkgs
+      aptlog "installed -> " + pkgs
+    else
       err 'DEB installation failed: ' + pkgs
       aptlog "fail -> " + pkgs
       warn "Check /tmp/mother_apt.log for output"
       exit 1
-    else
-      aptlog "installed -> " + pkgs
     end
   rescue Exception => e
     err 'DEB installation failed: ' + pkgs
