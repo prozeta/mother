14806f65a8f3fdd6c5c3af8a3e7e5f9993b9fc88
jan kaufman <jan.kaufman@prozeta.eu>
2016-06-01 22:12:15 +0200

    give verbose output

--- baseimage/build.sh
+++ baseimage/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -e
-
 b 'saving required versions into /etc/mother-versions...'
 echo "# generated on image build
 export PUPPET_VERSION=${PUPPET_VERSION}
@@ -20,12 +19,14 @@ fi
 b 'adding repositories...'
 echo "deb http://deb.theforeman.org/ trusty ${FV[0]}.${FV[1]}" > /etc/apt/sources.list.d/foreman.list
 echo "deb http://deb.theforeman.org/ plugins ${FV[0]}.${FV[1]}" >> /etc/apt/sources.list.d/foreman.list
-curl -q http://deb.theforeman.org/pubkey.gpg 2>/dev/null | apt-key add - >/dev/null
-curl -q https://apt.puppetlabs.com/${PUPPET_REL_PKG_NAME}.deb -o/tmp/${PUPPET_REL_PKG_NAME}.deb 2>/dev/null
-dpkg -i /tmp/${PUPPET_REL_PKG_NAME}.deb >/dev/null 2>&1
+curl http://deb.theforeman.org/pubkey.gpg | apt-key add -
+curl https://apt.puppetlabs.com/${PUPPET_REL_PKG_NAME}.deb -o/tmp/${PUPPET_REL_PKG_NAME}.deb
+dpkg -i /tmp/${PUPPET_REL_PKG_NAME}.deb
 rm -f /tmp/${PUPPET_REL_PKG_NAME}.deb
 bl 'done'
 
 b 'updating repositories...'
 apt-get -qqy update
+apt-get dist-upgrade -yyyy
+apt-get clean
 bl 'done'
